e649d83d2741bdbd32b1dfe3204c4514783308fb
sbalan7 <sbala7777@gmail.com>
2021-01-16 21:48:07 +0530

    getting there

--- get_single_cluster.py
+++ get_single_cluster.py
@@ -4,31 +4,16 @@ import sys
 
 
 VizieR_ROOT = 'https://vizier.u-strasbg.fr/viz-bin/'
-CG_PATH = 'VizieR-3?-source=J/A%2bA/618/A93/table1'
-OPTIONS = '&-out.max=50&-out.form=HTML%20Table&-out.add=_r&-out.add=_RAJ,_DEJ&-sort=_r&-oc.form=sexa'
+OPTIONS = 'VizieR-6?-out.form=%2bH&-source=J/A%2bA/618/A93&Cluster='
+
+targetcluster = sys.argv[1]
 
 # Open VizieR website
 driver = webdriver.Firefox()
-driver.get(VizieR_ROOT+CG_PATH+OPTIONS)
+driver.get(VizieR_ROOT+OPTIONS+targetcluster)
 
-# Submit request for catalogue
 driver.find_element_by_xpath("//select[@name='-out.max']/option[text()='unlimited']").click()
-element = driver.find_element_by_xpath(".//*[@id='vcst']")
-element.submit()
+element = driver.find_element_by_xpath("/html/body/div[4]/div/form/div[1]/div/div/div[2]/div[2]/div/table/tbody/tr/td/input").click()
 
-time.sleep(30)
-
-# Make a list of all the clusters from the table
-targetcluster = sys.argv[1]
-
-table = driver.find_element_by_css_selector("#c36180093t1_1")
-
-for row in table.find_elements_by_css_selector('tr'):
-    x = 0
-    for d in row.find_elements_by_css_selector('td'):
-        x += 1
-        if (x == 4):
-            if d.text == targetcluster:
-                print(d.get_attribute("href"))
-            break
-
+time.sleep(15)
+driver.quit()
