3338a88cd382b9e588a83fc0c5e84ab5011abc40
sbalan7 <sbala7777@gmail.com>
2021-01-23 00:18:05 +0530

    updated

--- get_multiple_clusters.py
+++ get_multiple_clusters.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 import matplotlib.pyplot as plt
 import time
-import sys
 import os
 
 
@@ -14,7 +13,7 @@ def get_cluster_list():
         
     return clusters
 
-def collect_data(targetcluster):
+def collect_data(targetcluster, threshold=0.3):
     # Open VizieR website
     driver = webdriver.Firefox()
     driver.get(VizieR_ROOT+OPTIONS+targetcluster)
@@ -28,30 +27,38 @@ def collect_data(targetcluster):
 
     gmag = []
     bprp = []
+    ug = []
+    ub = []
+
     for row in table.find_elements_by_css_selector('tr'):
         x = 0
         for d in row.find_elements_by_css_selector('td'):
             x += 1
-            if (x ==11):
+            if (x == 11):
                 if len(d.text) <= 1:
                     continue
                 gmag.append(float(d.text))
-            if (x==12):
+            if (x == 12):
                 if len(d.text) <= 1:
                     gmag.pop()
                     continue
                 bprp.append(float(d.text))
+            if (x == 13):
+                if float(d.text) > threshold:
+                    ub.append(bprp[-1])
+                    ug.append(gmag[-1])
     
     driver.quit()
+    
+    if len(bprp) <= 10:
+        print('Cluster not found')
 
-    if len(gmag) <= 10:
-        raise ValueError('Cluster not found')
+    return bprp, gmag, ub, ug
 
-    return bprp, gmag
-
-def plot(bprp, gmag, cluster):
+def plot(bprp, gmag, ub, ug, cluster):
     fig = plt.figure(figsize=(10, 6))
-    s = plt.scatter(bprp, gmag)
+    s = plt.scatter(ub, ug, color='blue', marker='o')
+    s = plt.scatter(bprp, gmag, color='red', marker='.')
     ax = s.axes
     ax.invert_yaxis()
     ax.set_xlabel(r'$BP-RP$', fontsize=12)
@@ -60,6 +67,7 @@ def plot(bprp, gmag, cluster):
     ax.grid(True)
     fig.tight_layout()
     plt.savefig(os.getcwd()+'/plots/'+cluster+'.png')
+    plt.close(fig)
 
 
 clusters = get_cluster_list()
@@ -68,7 +76,7 @@ for cluster in clusters:
     print(f'Collecting data for {cluster}')
     tic = time.time()
     target = cluster.replace(' ', '_')
-    b, g = collect_data(target)
-    plot(b, g, cluster)
+    b, g, ub, ug = collect_data(target)
+    plot(b, g, ub, ug, cluster)
     toc = time.time()
     print(f'Completed in {toc-tic} sec(s)')
