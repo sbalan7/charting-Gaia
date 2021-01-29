8e189a2a44539038e32496c3e5f1240638ed6c44
sbalan7 <sbala7777@gmail.com>
2021-01-29 09:04:36 +0530

    added cluster data

--- get_multiple_clusters.py
+++ get_multiple_clusters.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 import matplotlib.pyplot as plt
 import time
+import csv
 import os
 
 
@@ -17,40 +18,25 @@ def collect_data(targetcluster, driver, threshold=0.3):
     # Open VizieR website
     driver.get(VizieR_ROOT+OPTIONS+targetcluster)
 
+    headers = ['SlNo', 'RA_ICRS', 'DE_ICRS', 'Source', 'GLON', 'GLAT', 'plx', 'pmRA', 'pmDE', 'o_', 'GMag', 'BP-RP', 'PMemb', 
+                'Cluster', 'SimbadName', '_RA.icrs', '_DE.icrs']
+
     driver.find_element_by_xpath("//select[@name='-out.max']/option[text()='unlimited']").click()
     element = driver.find_element_by_xpath("/html/body/div[4]/div/form/div[1]/div/div/div[2]/div[2]/div/table/tbody/tr/td/input").click()
 
     time.sleep(7)
 
     table = driver.find_element_by_css_selector("#c36180093members_2")
-
-    gmag = []
-    bprp = []
-    prog = []
-    prob = []
-
-    for row in table.find_elements_by_css_selector('tr'):
-        x = 0
-        for d in row.find_elements_by_css_selector('td'):
-            x += 1
-            if (x == 11):
-                if len(d.text) <= 1:
-                    continue
-                gmag.append(float(d.text))
-            if (x == 12):
-                if len(d.text) <= 1:
-                    gmag.pop()
-                    continue
-                bprp.append(float(d.text))
-            if (x == 13):
-                if float(d.text) > threshold:
-                    prob.append(bprp[-1])
-                    prog.append(gmag[-1])
-        
-    if len(bprp) <= 10:
-        print('Cluster not found')
-
-    return bprp, gmag, prob, prog
+    
+    with open(os.getcwd()+'/clusters/'+targetcluster+'.csv', 'a') as f:
+        cl_writer = csv.writer(f, delimiter=',')
+        cl_writer.writerow(headers)
+        for row in table.find_elements_by_css_selector('tr'):
+            row_el = []
+            for d in row.find_elements_by_css_selector('td'):
+                row_el.append(d.text)
+            cl_writer.writerow(row_el)
+    
 
 def plot(bprp, gmag, prob, prog, cluster):
     fig = plt.figure(figsize=(14, 8))
@@ -67,8 +53,7 @@ def plot(bprp, gmag, prob, prog, cluster):
     plt.savefig(os.getcwd()+'/plots/'+cluster+'.png')
     plt.close(fig)
 
-
-def main():
+def open_VizieR():
     driver = webdriver.Firefox()
     clusters = get_cluster_list()
 
@@ -76,12 +61,14 @@ def main():
         print(f'Collecting data for {cluster}')
         tic = time.time()
         target = cluster.replace(' ', '_')
-        b, g, ub, ug = collect_data(target, driver)
-        plot(b, g, ub, ug, cluster)
+        collect_data(target, driver)
         toc = time.time()
         print(f'Completed in {toc-tic} sec(s)')
 
     driver.quit()
+
+def main():
+    open_VizieR()    
 
 if __name__ == '__main__':
     main()
