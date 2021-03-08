18cf7005c39a0d29d7833a537a1d84f6a8e08bfb
sbalan7 <sbala7777@gmail.com>
2021-03-08 10:43:12 +0530

    fixed erroneous radius calc

--- plot_king.py
+++ plot_king.py
@@ -26,16 +26,16 @@ def load_data(cluster_name):
 def bin_stars(bins, cluster_name):
     clu_df = load_data(cluster_name)
     
-    _ = [np.sqrt(i+1) for i in range(bins)]
-    base_r = max(clu_df['Rad_Dist']) / np.sum(_)
-
+    base_r = max(clu_df['Rad_Dist']) / np.sqrt(bins)
+    
     counts = []
     cu_counts = []
 
     for i in range(bins): 
         curr_r = np.sqrt(i+1) * base_r
+        print(curr_r)
         cu_counts.append(np.sum(clu_df['Rad_Dist']<curr_r))
-
+    
     for i in range(len(cu_counts)):
         if i > 0:
             counts.append(cu_counts[i] - cu_counts[i-1])
@@ -44,16 +44,15 @@ def bin_stars(bins, cluster_name):
     
     return counts, base_r
 
-def plot_kings_profile(counts, base_r):
+def plot_kings_profile(counts, base_r, cluster_name):
     bins = len(counts)
     print(counts)
-    print(base_r)
     amplitude = counts[0]/(np.pi*base_r*base_r)
-    r_c = 2.16
+    r_c = 7.59
     fig = plt.figure()
     ax = plt.gca()
 
-    for r_t in [10]:
+    for r_t in [85, 95, 105]:
         r = np.linspace(5e-3, r_t, 100)
         kp = KingProjectedAnalytic1D(amplitude, r_c, r_t)
         sig = kp(r)
@@ -61,19 +60,21 @@ def plot_kings_profile(counts, base_r):
         radii = np.array([np.sqrt(i+1)*base_r for i in range(bins)])
         surf_d = counts / (radii*radii*np.pi)
     
-        ax.plot(r, sig/sig[0])
-        ax.scatter(radii, surf_d, marker='.')
+        ax.plot(r, sig/sig[0], label=str(r_t))
+        ax.scatter(radii, surf_d, marker='.', color='#39ff14')
         
         ax.set_yscale('log')
         ax.set_xscale('log')
-        ax.set_ylim([5e-3, 1e0])
-        ax.set_xlim([0.8, 10])
+        ax.set_ylim([1e-5, 1e-1])
+        ax.set_xlim([10, 400])
     
     plt.xlabel('log(radii)')
     plt.ylabel('log(surface density)')
+    plt.legend()
+    plt.title(f'{cluster_name}, $r_c = {r_c}$')
     plt.show()
 
-cluster_name = 'NGC_2287'
-counts, base_r = bin_stars(22, cluster_name)
-plot_kings_profile(counts, base_r)
+cluster_name = 'NGC_752'
+counts, base_r = bin_stars(25, cluster_name)
+plot_kings_profile(counts, base_r, cluster_name)
 
