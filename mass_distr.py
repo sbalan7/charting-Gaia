37f1624bc03a2b3af482f3027b23eaf956eb9795
sbalan7 <sbala7777@gmail.com>
2021-04-13 15:04:32 +0530

    polynomial precision

--- mass_distr.py
+++ mass_distr.py
@@ -6,6 +6,7 @@ import json
 import sys
 import os
 
+
 with open('select.json') as f:
     data = json.load(f)
 
@@ -14,21 +15,22 @@ lgmag, hgmag = 10, 18
 for cluster_name, subdata in data.items():
     iso_df, clu_df = load_data(cluster_name)
     poly, main_seq = find_mass_fn(iso_df)
-    int_poly = np.polyint(poly)
+    data[cluster_name]["mass_fn"] = list(poly)
     
+    int_poly = np.polyint(poly)
     avg_mass = ((int_poly(hgmag) - int_poly(lgmag)) / (hgmag - lgmag))
+    
     xs = np.linspace(lgmag, hgmag, 100)
     ys = poly(xs)
-    
+
     plt.scatter(main_seq['Gmag'], main_seq['Mass'], label="Actual Data")
     plt.plot(xs, ys, label=f'${poly[0]:.3f}+{poly[1]:.3f}x+{poly[2]:.3f}x^2+{poly[3]:.3f}x^3$')
+
     plt.title(f'{cluster_name}\'s Mass v Gmag Function \nAverage Mass = {avg_mass}')
     plt.xlabel('Gmag')
     plt.ylabel('Mass')
     plt.legend()
     plt.show()
-    data[cluster_name]["mass_fn"] = list(poly)
-
 
 with open('select.json', 'w') as f:
     json.dump(data, f, indent=4)
